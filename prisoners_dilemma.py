7286180e90021125bb1cf63014aebbe4602c98fe
moritzemm <emmamor2323>
2020-02-11 13:49:35 -0600

    2/11/2020

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -190,7 +190,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
     elif player == 4:
         if getting_team_name:
-            return 'Enter Team Name Here'
+            return 'Em&Cal'
         else:
             if len(opponent_history)==0:
                 return 'b'
