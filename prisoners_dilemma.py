cdebdf79f4778af8b157e5036f2d0c2ba59bb438
dautom21 <dautom@urbandaleschools.com>
2021-02-10 08:21:28 -0600

    Prisoners Dilemma Finished

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -189,12 +189,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
         if getting_team_name:
             return 'Tom Dau'
         else:
-            if len(opponent_history)==0: #It's the first round: collude
+            if len(opponent_history)==0:  #It's the first round: collude
                 return 'c'
             elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray if they were severely punished last time
+                return 'b'  # betray if they were severely punished last time
+            elif history[-1]=='b':
+                return 'b'
+            elif history[-1]=='b' and opponent_history[-1]=='b':
+                return 'c'
             else:
-                return 'c' #otherwise collude
+                return 'c'  #otherwise collude
 
 
 
