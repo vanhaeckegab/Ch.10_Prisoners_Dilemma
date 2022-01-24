25e994ed0289e89598886340a2b7d792a2a7fbe6
Flare Flare Flare <flyrmat@urbandaleschools.com>
2022-01-24 13:13:10 -0600

    done1

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -248,7 +248,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
     elif player == 6:
         if getting_team_name:
-            return 'unoffical1'
+            return 'unoffical2'
         else:
             if len(opponent_history) == 0:  # It's the first round: collude
                 return 'c'
