208d38fc49b744a4df0abbb50c59aa51d5e67650
bakerkai <bakerkai@urbandaleschools.com>
2022-01-23 18:04:44 -0600

    good enough

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -162,9 +162,15 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
             elif history[-1]=='c' and opponent_history[-1]=='c':
-                return 'c' # betray if they were severely punished last time
+                return 'c' # If they are colluding, collude with them or else betray
+            elif history[-1]=='b' and opponent_history[-1]=='c':
+                return 'b' # If they are colluding, collude with them or else betray
+            elif history[-1]=='c' and opponent_history[-1]=='b':
+                return 'b' # If they are colluding, collude with them or else betray \
+            elif history[0]=='b' and opponent_history[0]=='c':
+                return 'b' # If they are colluding, collude with them or else betray
             else:
-                return 'c' #otherwise collude
+                return 'b' #otherwise betray
 
 
 
